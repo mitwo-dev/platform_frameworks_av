477c0f7089d9a31ce166bc7e6f1d96024ca2346c
Sergii Doroshenko <x0163979@ti.com>
2012-07-20 16:46:23 -0500

    AACExtractor: Added support for APE tag
    To support aac audio with APE tag we need parse
    this tag. Otherwise AACExtractor stops reading of
    audio file.
    
    DR: OMAPS00267689
    
    Change-Id: Ic697b90dbc9f2b93aeb227411359c36c113cb71e
    Signed-off-by: Daniel Levin <dendy@ti.com>

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -95,6 +95,7 @@ LOCAL_SRC_FILES:=                         \
         ExtendedExtractor.cpp             \
         ExtendedUtils.cpp                 \
         ExtendedStats.cpp                 \
+        APE.cpp                           \
 
 LOCAL_C_INCLUDES:= \
         $(TOP)/frameworks/av/include/media/ \

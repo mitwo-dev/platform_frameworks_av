465e8f1cbde0acab5d5f62c77ec827d2bf93427e
Pavel Kirpichyov <pavel.kirpichyov@gmail.com>
2012-12-29 15:11:56 +0400

    Fix typo
    Change-Id: I179187b33f666dff88b57a4c24d7507eee0219cd
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -306,7 +306,7 @@ uint32_t OMXCodec::getComponentQuirks(
         quirks |= kOutputBuffersAreUnreadable;
     }
     if (list->codecHasQuirk(
-                index, "requies-loaded-to-idle-after-allocation")) {
+                index, "requires-loaded-to-idle-after-allocation")) {
         quirks |= kRequiresLoadedToIdleAfterAllocation;
     }
 #ifdef QCOM_HARDWARE
